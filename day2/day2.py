b0609d303a423c83fe1c350321e6d9ef62826f2f
Genghis <gphilip@kipsu.com>
2024-12-02 12:38:32 -0600

    refactored retry logic into handler

--- day2/day2.py
+++ day2/day2.py
@@ -5,23 +5,25 @@ def handler(part):
     safe = 0
     for i in dataset:
         report = [int(x) for x in i.split()]
-        safe += safecheck(report, part)
+        addition = safecheck(report)
+        if addition:
+            safe += addition
+        elif part == 2:
+            for i in range(len(report)):
+                newreport = list(report)
+                del newreport[i]
+                if safecheck(newreport):
+                    safe += 1
+                    break
     print(safe)
     
-def safecheck(report,rerun):
+def safecheck(report):
     safechecklist = [x[0]-x[1] for x in zip(report[0::],report[1::])]
     if all(4 > x > 0 for x in safechecklist) or all(0 > x > -4 for x in safechecklist):
         return 1
-    elif rerun == False:
-        for i in range(len(report)):
-            newreport = list(report)
-            del newreport[i]
-            if safecheck(newreport, True):
-                return 1
-        return 0
     else:
         return 0
 
 if __name__ == "__main__":
-    handler(True) #runs part 1
-    handler(False) #runs part 2
+    handler(1) #runs part 1
+    handler(2) #runs part 2
