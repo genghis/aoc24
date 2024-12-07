7da79a98455be5bc3eaf9d777168dfdc5113cdcc
Genghis <gphilip@kipsu.com>
2024-12-07 12:17:07 -0600

    finally finished 5.2

--- day05/day5.py
+++ day05/day5.py
@@ -1,34 +1,5 @@
-# from aocd import get_data
-# ruleslist,updateslist = get_data(day=5, year=2024).split("\n\n")
-
-ruleslist, updateslist = '''47|53
-97|13
-97|61
-97|47
-75|29
-61|13
-75|53
-29|13
-97|29
-53|29
-61|53
-97|53
-61|29
-47|13
-75|47
-97|75
-47|61
-75|61
-47|29
-75|13
-53|13
-
-75,47,61,53,29
-97,61,53,29,13
-75,29,13
-75,97,47,61,53
-61,13,29
-97,13,75,29,47'''.split("\n\n")
+from aocd import get_data
+ruleslist,updateslist = get_data(day=5, year=2024).split("\n\n")
 
 rules = ruleslist.splitlines()
 updates = updateslist.splitlines()
@@ -43,25 +14,22 @@ for rule in rules:
     else:
         rulesorder[before] = [after]
 
-def makevalid(numberlist):
-    newline = numberlist.copy()
-    for index, value in enumerate(numberlist):
-            if value in rulesorder:
-                for i in rulesorder[value]:
-                    if i in numberlist[0:index]:
-                        if value in newline:
-                            newline.remove(value)
-                        offenderlocation = numberlist.index(i)
-                        newline.insert(offenderlocation-1, value)
-            else:
-                if value not in newline:
-                    newline.insert(-1, value)
-    print(newline)
-    return newline
-
-def part1():
-    count = 0
-
+def makevalid(numberlist, newline):
+    if not numberlist:
+        return newline
+    for i in numberlist:
+        workinglist = [x for x in numberlist if x != i]
+        if i in rulesorder:
+            if all(x in rulesorder[i] for x in workinglist):
+                newline.append(i)
+                return(makevalid(workinglist,newline))
+        elif len(numberlist) == 1:
+            newline.append(i)
+            return(makevalid(workinglist,newline))
+  
+def handler():
+    validcount = 0
+    invalidcount = 0
     for update in updates:
         numberlist = [int(x) for x in update.split(',')]
         valid = True
@@ -73,32 +41,14 @@ def part1():
                     else:
                         valid = False
                         break
+        middle = int((len(numberlist)+1)/2)-1
         if valid:
-            middle = int((len(numberlist)+1)/2)-1
-            count += numberlist[middle]
-
-    return count
-
-def part2():
-    count = 0
-    invalidlines = []
-    for update in updates:
-        numberlist = [int(x) for x in update.split(',')]
-        valid = True
-        for index, value in enumerate(numberlist):
-            if value in rulesorder:
-                for i in rulesorder[value]:
-                    if i not in numberlist[0:index]:    
-                        pass
-                    else:
-                        valid = False
-                        break
+            validcount += numberlist[middle]
         if not valid:
-            middle = int((len(numberlist)+1)/2)-1
-            count += makevalid(numberlist)[middle]
-    return count
-                    
-            
+            invalidcount += makevalid(numberlist,[])[middle]
+    return (validcount,invalidcount)
+
 if __name__ == "__main__":
-    print(f"Part 1: {part1()}")
-    print(f"Part 2: {part2()}")
+    results = handler()
+    print(f"Part 1: {results[0]}")
+    print(f"Part 2: {results[1]}")
