af357d834e8b4f2cb98f38a0ae7f52ce6509c3e6
Genghis Philip <genghis@Tharkad.local>
2024-12-09 23:02:03 -0600

    part1

--- day09/day9.py
+++ day09/day9.py
@@ -3,33 +3,43 @@ dataset = get_data(day=9, year=2024)
 
 # dataset = "2333133121414131402"
 
-disk = [list(str(int(idx/2))*int(x)) if idx % 2 == 0 else list('.'*int(x)) for idx, x in enumerate(dataset)]
-diskmap = [x for y in disk for x in y]
-# print(diskmap)
+diskmap = [int(idx / 2) if idx % 2 == 0 else '.' for idx, val in enumerate(dataset) for _ in range(int(val))]
 
 def rearranger(disklist):
+    count = 0
     tempdisk = disklist.copy()
-    backindex = -1
-    forwardindex = tempdisk.index('.')
-    while abs(backindex) <= tempdisk.count('.'):
-        if disklist[-1] != '.':
-            lastspotvalue = disklist[backindex]
-            tempdisk[forwardindex] = lastspotvalue
-            tempdisk[backindex] = '.'
-        backindex -= 1
-        forwardindex = tempdisk.index('.')
-    return tempdisk
+    for i, v in enumerate(tempdisk):
+            if v == '.':
+                while tempdisk[-1] == '.':
+                    tempdisk.pop()
+                if i >= len(tempdisk):
+                    break
+                tempdisk[i] = tempdisk[-1]
+                tempdisk.pop()
+            count+=int(tempdisk[i])*i
+        
+    return count
+
+def file_rearranger(disklist):
+    count = 0
+    tempdisk = disklist.copy()
+    for i, v in enumerate(tempdisk):
+            if v == '.':
+                while tempdisk[-1] == '.':
+                    tempdisk.pop()
+                if i >= len(tempdisk):
+                    break
+                tempdisk[i] = tempdisk[-1]
+                tempdisk.pop()
+            count+=int(tempdisk[i])*i
+        
+    return count
 
 def part1():
-    total = 0
-    calclist = rearranger(diskmap)
-    print(diskmap)
-    print(''.join(calclist))
-    for ind, value in enumerate(calclist):
-        if value != '.':
-            # print(int(value)*ind)
-            total += int(value)*ind
-    return total
+    return rearranger(diskmap)
+
+def part2():
+    return file_rearranger(diskmap)
 
 if __name__ == "__main__":
     print(f"Part 1: {part1()}")
