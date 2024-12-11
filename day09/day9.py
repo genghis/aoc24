5ef98ded010e2fff98d779494c5195259ac0a179
Genghis <gphilip@kipsu.com>
2024-12-11 14:17:19 -0600

    adds day10

--- day09/day9.py
+++ day09/day9.py
@@ -1,45 +1,38 @@
 from aocd import get_data
-dataset = get_data(day=9, year=2024)
+# dataset = get_data(day=9, year=2024)
 
-# dataset = "2333133121414131402"
+dataset = "2333133121414131402"
 
 diskmap = [int(idx / 2) if idx % 2 == 0 else '.' for idx, val in enumerate(dataset) for _ in range(int(val))]
 
-def rearranger(disklist):
-    count = 0
-    tempdisk = disklist.copy()
-    for i, v in enumerate(tempdisk):
-            if v == '.':
-                while tempdisk[-1] == '.':
-                    tempdisk.pop()
-                if i >= len(tempdisk):
-                    break
-                tempdisk[i] = tempdisk[-1]
-                tempdisk.pop()
-            count+=int(tempdisk[i])*i
-        
-    return count
-
-def file_rearranger(disklist):
-    count = 0
-    tempdisk = disklist.copy()
-    for i, v in enumerate(tempdisk):
-            if v == '.':
-                while tempdisk[-1] == '.':
-                    tempdisk.pop()
-                if i >= len(tempdisk):
-                    break
-                tempdisk[i] = tempdisk[-1]
-                tempdisk.pop()
-            count+=int(tempdisk[i])*i
-        
-    return count
-
 def part1():
-    return rearranger(diskmap)
+    count = 0
+    tempdisk = diskmap.copy()
+    for i, v in enumerate(tempdisk):
+            if v == '.':
+                while tempdisk[-1] == '.':
+                    tempdisk.pop()
+                if i >= len(tempdisk):
+                    break
+                tempdisk[i] = tempdisk[-1]
+                tempdisk.pop()
+            count+=int(tempdisk[i])*i
+    return count
 
 def part2():
-    return file_rearranger(diskmap)
+    count = 0
+    tempdisk = diskmap.copy()
+    for i, v in enumerate(tempdisk):
+            if v == '.':
+                while tempdisk[-1] == '.':
+                    tempdisk.pop()
+                if i >= len(tempdisk):
+                    break
+                tempdisk[i] = tempdisk[-1]
+                tempdisk.pop()
+            count+=int(tempdisk[i])*i
+    return count
 
 if __name__ == "__main__":
     print(f"Part 1: {part1()}")
+    print(f"Part 2: {part2()}")
--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,51 @@
 from aocd import get_data
 dataset = get_data(day=10, year=2024).splitlines()
 
-pass
+trailheads = {}
+trailheads2 = {}
+trailends = []
+
+for ycoord, row in enumerate(dataset):
+    for xcoord, value in enumerate(row):
+        if int(value) == 0:
+            trailheads[(ycoord,xcoord)] = set()
+            trailheads2[(ycoord,xcoord)] = []
+        elif int(value) == 9:
+            trailends.append((ycoord,xcoord))
+
+def check_surrounding(starting, coordinates, value):
+    if value == 0:
+        trailheads[coordinates].add(starting)
+        trailheads2[coordinates].append(starting)
+    surrounding = {}
+    ycoord,xcoord = coordinates
+    if xcoord > 0:
+        surrounding['left'] = (ycoord, xcoord-1)
+    if xcoord < len(dataset[0])-1:
+        surrounding['right'] = (ycoord, xcoord+1)
+    if ycoord > 0:
+        surrounding['up'] = (ycoord-1, xcoord)
+    if ycoord < len(dataset)-1:
+        surrounding['down'] = (ycoord+1, xcoord)
+    for i in surrounding:
+        yc, xc = surrounding[i]
+        if int(dataset[yc][xc]) == value-1:
+            check_surrounding(starting, (yc,xc), value-1)
+
+def part1():
+    count = 0
+    for i in trailends:
+        check_surrounding(i,i,9)
+    for j in trailheads.values():
+        count += len(j)
+    return count
+
+def part2():
+    count = 0
+    for i in trailheads2.values():
+        count += len(i)
+    return count
+
+if __name__ == '__main__':
+    print(f'Part 1: {part1()}')
+    print(f'Part 2: {part2()}')
